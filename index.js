7d79f1ceb1ba8a96238af410efed768ba08ff983
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-10-14 11:12:29 +0300

    Events

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+//Move dodger right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  const containerWidth = 400;
+
+  if (left < containerWidth - dodger.offsetWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }else if(e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
